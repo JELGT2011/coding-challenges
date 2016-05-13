567aa026a3a411a3986d2562c82d4f17c8ab11cd
Jason Libbey <jelgt2011@gmail.com>
2016-05-13 17:30:01 -0400

    init

---
+++ Algorithms/Dynamic Programming/Knapsack.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+var cache map[int]int
+var weights map[int]bool
+
+func knapsack(k int) int {
+	if val, ok := cache[k]; ok {
+		return val
+	}
+
+	var s int
+	best := k
+	for weight := range weights {
+		if k-weight >= 0 {
+			s = knapsack(k - weight)
+			if s < best {
+				best = s
+			}
+		}
+	}
+	cache[k] = best
+	return cache[k]
+}
+
+func main() {
+	var T, N, K int
+	var weight int
+	fmt.Scanf("%d", &T)
+	for t := 0; t < T; t++ {
+		fmt.Scanf("%d %d", &N, &K)
+		cache = make(map[int]int)
+		weights = make(map[int]bool)
+		for n := 0; n < N; n++ {
+			fmt.Scanf("%d", &weight)
+			weights[weight] = true
+		}
+		fmt.Println(K - knapsack(K))
+	}
+}
